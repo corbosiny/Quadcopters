89d68b8993f74bd63cd57d1c476fcd274de0a7eb
Corey Hulse <cohulse@asu.edu>
2016-12-16 22:38:35 -0600

    Minor bug fixes and Full Comments

--- quadMotorController.ino
+++ quadMotorController.ino
@@ -1,14 +1,22 @@
+//----------------------------------------------\\
+//-********************************************-\\
+//-***     Feromone Motor Controller        ***-\\
+//-***  Controlls and tracks motorspeeds    ***-\\
+//-***         Subteam: Corey               ***-\\
+//-********************************************-\\
+//----------------------------------------------\\
+
 #include <Servo.h>
 
-int motorPins[] = {};
-int motorSpeedOffsets[] = {0,0,0,0};
-int currentMotorSpeeds[] = {0, 0, 0, 0};
+int motorPins[] = {};                                                                                     //Signal pin for each of the four motors                                                            
+int motorSpeedOffsets[] = {0,0,0,0};                                                                      //respective offsets for each motor on the drone, this is to help them run at the same speed
+int currentMotorSpeeds[] = {0, 0, 0, 0};                                                                  //keeps track of the current motors speeds, you will see why we use this later
 
-Servo motors[4];
+Servo motors[4];                                                                                          //The ESCs take signals similar to servos so we can make use of the arduino library for them
 void setup() 
 {
 
-  for(int i = 0; i < 4; i++) {motors[i].attach(motorPins[i]);}
+  for(int i = 0; i < 4; i++) {motors[i].attach(motorPins[i]);}                                            //attaches each pin to its respective motor
   
 }
 
@@ -17,24 +25,24 @@ void loop()
 
 }
 
-void writeMotor(int motorNumber, int newSpeed)
+void writeMotor(int motorNumber, int newSpeed)                                                            //sets a new speed for an individual motor and updates the current speed variable
 {
-  currentMotorSpeeds[motorNumber - 1] = newSpeed - motorSpeedOffsets[motorNumber - 1];
-  motors[motorNumber - 1].writeMicroseconds(currentMotorSpeeds[motorNumber - 1]);
+  currentMotorSpeeds[motorNumber - 1] = newSpeed + motorSpeedOffsets[motorNumber - 1];                    //adds the offset to the entered speed before writing it to the motor
+  motors[motorNumber - 1].writeMicroseconds(currentMotorSpeeds[motorNumber - 1]);                         //writeMicroseconds is used instead of write as writeMicroseconds works from 1000 to 2000 instead of 0 to 180 like write, therefor more precise speed options
 }
 
-void writeMotors(int speeds[])
+void writeMotors(int speeds[])                                                                            //takes an array of speeds and writes all motors to the respective speed assigned them
 {
 
   for(int i = 0; i < 4; i++) 
   {
-    motors[i].writeMicroseconds(speeds[i] - motorSpeedOffsets[i]); 
-    currentMotorSpeeds[i] = speeds[i] - motorSpeedOffsets[i];
+    motors[i].writeMicroseconds(speeds[i] - motorSpeedOffsets[i]);                                        //writing each motor to their respective speed and updating the speed variables while factoring in the motor offsets
+    currentMotorSpeeds[i] = speeds[i] - motorSpeedOffsets[i];                                             
   }
   
 }
 
-void adjustMotors(int adjustments[])
+void adjustMotors(int adjustments[])                                                                      //takes in an adjustment and will add that to the current speed of the motor, this is why we keep track of their current speeds
 {
 
    for(int i = 0; i < 4; i++) {currentMotorSpeeds[i] += adjustments[i];}
@@ -42,9 +50,9 @@ void adjustMotors(int adjustments[])
 
 }
 
-void adjustMotor(int motorNumber, int adjustment) {writeMotor(motorNumber, currentMotorSpeeds[motorNumber - 1] + adjustment);}
+void adjustMotor(int motorNumber, int adjustment) {writeMotor(motorNumber, currentMotorSpeeds[motorNumber - 1] + adjustment);} //adjust just one motor
 
-void motorTest()
+void motorTest()                                                                                                               //just writes a bunch of values to the motors just so we can see if they are working
 {
 
   for(int i = 0; i < 1000; i += 10) 
